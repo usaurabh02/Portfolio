bbfc2338a545b9ff0aa7101998cd362d78020557
saurabh <saurabhu154@gmail.com>
2024-03-28 13:03:01 +0530

    changed scroll text size

--- portfolio/src/components/Overlay.jsx
+++ portfolio/src/components/Overlay.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Overlay = () => {
   return (
     <div
-      className="absolute top-[5%] left-[3%] text-black"
+      className="absolute top-[5%] left-[3%] text-[20px] text-black"
       style={{
         fontFamily: "'Rubik'",
       }}
