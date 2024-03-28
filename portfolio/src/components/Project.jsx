c4548c08549569d9a31991627b41ccc9c6ea6abd
saurabh <saurabhu154@gmail.com>
2024-03-28 11:54:35 +0530

    added icon and title for portfolio

--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -6,11 +6,14 @@ import DataA from "../assets/daaa.png";
 import Dlp from "../assets/dlp.png";
 import Mlp from "../assets/ml1.png";
 
-
 const ProjectItem = ({ image, name, organisation, verify }) => {
   return (
     <div className=" bg-white border border-gray-200 rounded-lg shadow-xl ">
-      <img className="rounded-t-lg w-[500px] h-[250px] bg-transparent" src={image} alt="" />
+      <img
+        className="rounded-t-lg w-[500px] h-[250px] bg-transparent"
+        src={image}
+        alt=""
+      />
       <div className="p-5">
         <p
           className="text-[20px] mb-2"
@@ -37,24 +40,24 @@ const ProjectItem = ({ image, name, organisation, verify }) => {
         >
           {organisation}
         </p>
-       
         <a href={verify} target="_blank" rel="noopener noreferrer">
-        <button className="inline-flex items-center p-2 mt-2 text-sm font-medium text-center text-white bg-[#6486B4] rounded-lg hover:bg-[#B0C9EB]">Visit
-          <svg
-          className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-          </svg>
+          <button className="inline-flex items-center p-2 mt-2 text-sm font-medium text-center text-white bg-[#6486B4] rounded-lg hover:bg-[#B0C9EB]">
+            Visit
+            <svg
+              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
           </button>
         </a>
       </div>
@@ -85,7 +88,7 @@ export const Project = () => {
               letterSpacing: 0.5,
             }}
           >
-           PROJECTS
+            PROJECTS
           </p>
         </div>
         <div
@@ -105,36 +108,42 @@ export const Project = () => {
             organisation="3D Website for Reealtech"
             verify={"https://reealtech.com/"}
           />
-        <ProjectItem
-        image={Vambox}
-        name="Vambox"
-        organisation="India's first free WebVR player for 360 content streaming."
-        verify={"https://vam-box.vercel.app/"}
-      />
-      <ProjectItem
-      image={Vamverse}
-      name="Vamverse"
-      organisation="3D Website for Vamverse with Virtual Try-On solutions."
-      verify={"https://vamverse.com/"}
-    />
-    <ProjectItem
-    image={Dlp}
-    name="Deep Learning Project"
-    organisation="Deep learning modal for safety detection in construction site."
-    verify={"https://drive.google.com/drive/u/6/folders/19FdN02fzce7kkhTonSJbNUxMJHjv4YhU"}
-  />
-    <ProjectItem
-    image={DataA}
-    name="Data Analysis Project"
-    organisation="Spatial & Geo-spatial data analysis."
-    verify={"https://drive.google.com/drive/u/6/folders/1fEbuqmd3cEx4GdGeQqnH0PHf3Rr-OK70"}
-  />
-<ProjectItem
-image={Mlp}
-name="Machine Learning Project"
-organisation="Face detection, age & gender prediction and emotion detection."
-verify={"https://drive.google.com/drive/u/6/folders/1R39V5RRV-4jjh6C7az-4uqIl-2Vnuss8"}
-/>
+          <ProjectItem
+            image={Vambox}
+            name="Vambox"
+            organisation="India's first free WebVR player for 360 content streaming."
+            verify={"https://vam-box.vercel.app/"}
+          />
+          <ProjectItem
+            image={Vamverse}
+            name="Vamverse"
+            organisation="3D Website for Vamverse with Virtual Try-On solutions."
+            verify={"https://vamverse.com/"}
+          />
+          <ProjectItem
+            image={Dlp}
+            name="Deep Learning Project"
+            organisation="Deep learning modal for safety detection in construction site."
+            verify={
+              "https://drive.google.com/drive/u/6/folders/19FdN02fzce7kkhTonSJbNUxMJHjv4YhU"
+            }
+          />
+          <ProjectItem
+            image={DataA}
+            name="Data Analysis Project"
+            organisation="Spatial & Geo-spatial data analysis."
+            verify={
+              "https://drive.google.com/drive/u/6/folders/1fEbuqmd3cEx4GdGeQqnH0PHf3Rr-OK70"
+            }
+          />
+          <ProjectItem
+            image={Mlp}
+            name="Machine Learning Project"
+            organisation="Face detection, age & gender prediction and emotion detection."
+            verify={
+              "https://drive.google.com/drive/u/6/folders/1R39V5RRV-4jjh6C7az-4uqIl-2Vnuss8"
+            }
+          />
         </div>
       </div>
     </div>
